885479a7780b418d736897162d3e0819296616f9
guna0223 <gunasekar0223@gmail.com>
2025-09-25 14:45:18 +0530

    added filed

---
+++ frontend/js/script.js
@@ -0,0 +1,7 @@
+// For future interactivity
+console.log("Welcome to Monsoon Days Platform!");
+
+// Example: Alert when user clicks button
+document.querySelector(".btn").addEventListener("click", () => {
+  alert("Let's start your journaling journey!");
+});
